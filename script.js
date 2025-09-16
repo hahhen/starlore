75e72c482613394a5b31d519f8179129163654e7
Hahhen <arthurmp2007@hotmail.com>
2025-09-16 14:01:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const wrap = document.querySelector('.img-wrap');
+const imgs = document.querySelectorAll('.img-wrap img');
+
+let idx = 0;
+
+function showImg() {
+    if (idx >= imgs.length) idx = 0;
+    if (idx < 0) idx = imgs.length - 1;
+    wrap.style.transform = `translateX(-${idx * 100}%)`;
+}
+
+next.addEventListener('click', () => {
+    idx++;
+    showImg();
+    clearInterval(interval); // Clear the old one
+    interval = setInterval(() => {
+        idx++;
+        showImg();
+    }, 3000);
+});
+
+prev.addEventListener('click', () => {
+    idx--;
+    clearInterval(interval); // Clear the old one
+    showImg();
+    interval = setInterval(() => {
+        idx++;
+        showImg();
+    }, 3000);
+});
+
+let interval = setInterval(() => {
+    idx++;
+    showImg();
+}, 3000);
+
+showImg();
